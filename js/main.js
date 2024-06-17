a3ac749633e96a7fbe57263e9bc0a7a51518aa37
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-17 14:21:20 +0200

    style: add comment for the logic

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@
 // Da lì parte un timer di 30 secondi.
 // Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+
+//creare un array con i numeri random:
+    // creare un arrai vuoto
+    // prendere la funzione che genera i numeri randomù
+    // inserire nell'array i numeri finché non si arriva a una lunghezza di 5
+    // se il numero già presente genera un altro
+
+// dopo che il browser ha eseguito questo compito legge una funzione con timer 30 sec
+// questa funzione dovrà fare:
+    // nascondere i numeri presenti in pagina
+    // chiedere 5 volte alla persona di inserimente dei numeri
+    // questi numeri verranno messi in un array ->
+        // array vuoto
+        // chiedo il numero
+        // lo inserisco
+    // confronto array della persona con l'array dei numeri random 
